52c48b3875968a1431a6022cf81f74abf6a50ea8
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2023-08-08 19:59:43 -0300

    Update main.js
    Added more dynamic features to the site

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
+const baseURL = "https://tarmeezacademy.com/api/v1";
+
 // API Get request for the post
-axios
-    .get("https://tarmeezacademy.com/api/v1/posts?limit=4")
-    .then(function (response) {
-        // Array for posts
-        let posts = response.data.data;
+axios.get(`${baseURL}/posts?limit=4`).then(function (response) {
+    // Array for posts
+    let posts = response.data.data;
 
-        // Post container
-        let postContainer = document.querySelector(".posts-container");
-        // handle success
+    // Post container
+    let postContainer = document.querySelector(".posts-container");
 
-        for (post of posts) {
-            // Let Author details
-            let author = post.author;
+    postContainer.innerHTML = "";
+    // handle success
 
-            // console.log(author.profile_image);
+    for (post of posts) {
+        // Let Author details
+        let author = post.author;
 
-            console.log(post);
-            let postContent = `
+        // console.log(author.profile_image);
+
+        console.log(post);
+        let postContent = `
             <div class="card mt-4 w-100 shadow" style="width: 18rem;">
                 <section class="d-flex align-items-center gap-2 p-3 bg-body-tertiary border-bottom border-2">
                     <img src="${
@@ -39,10 +41,10 @@ axios
                             <path
                                 d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                         </svg>
-                        <span>(3) comments</span>
+                        <span>(${post.comments_count}) comments</span>
                 </section>
             </div>`;
 
-            postContainer.innerHTML += postContent;
-        }
-    });
+        postContainer.innerHTML += postContent;
+    }
+});
