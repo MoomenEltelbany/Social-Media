48570a5d3e07ca1f80988749554fe35e7608453b
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2023-08-24 22:07:01 -0300

    Update postDetails.js

--- postDetails.js
+++ postDetails.js
@@ -27,15 +27,15 @@ function getClickedPost() {
                 <section class="p-3">
                     <img src="${postData.image}" class="card-img-top rounded-bottom" alt="Post-Image">
                     <div>
-                        <p class="fs-6 text-body-tertiary">2 min ago</p>
-                        <h5 class="card-title">Special Title treatment</h5>
-                        <p class="card-text pb-2 border-bottom border-2">Here we will add some title content</p>
+                        <p class="fs-6 text-body-tertiary">${postData.created_at}</p>
+                        <h5 class="card-title">Special ${postData.title}</h5>
+                        <p class="card-text pb-2 border-bottom border-2">${postData.body}</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-pen" viewBox="0 0 16 16">
                             <path
                                 d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z" />
                         </svg>
-                        <span>(3) comments</span>
+                        <span>(${postData.comments_count}) comments</span>
                 </section>
             </div>
             `;
